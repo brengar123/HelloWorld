79ef8ad16f780926f9cb2f2e483353448b861feb
garvin <garvinwang12@hotmail.com>
2020-03-15 13:06:42 +1100

    week 4 homework

--- app/src/main/java/com/example/helloworld/DetailActivity.java
+++ app/src/main/java/com/example/helloworld/DetailActivity.java
@@ -1,4 +1,76 @@
 package com.example.helloworld;
 
-public class DetailActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+    private TextView name;
+    private TextView shortName;
+    private TextView value;
+    private TextView change1h;
+    private TextView change24h;
+    private TextView change7d;
+    private TextView market;
+    private TextView volume;
+    private ImageView search;
+
+    private static final String TAG = "DetailActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        int position = intent.getIntExtra(MainActivity.EXTRA_MESSAGE, 0);
+        Coin coin = Coin.getCoins().get(position);
+
+        name = findViewById(R.id.mName);
+        shortName = findViewById(R.id.mSymbol);
+        value = findViewById(R.id.textView10);
+        change1h = findViewById(R.id.textView11);
+        change24h = findViewById(R.id.textView12);
+        change7d = findViewById(R.id.textView13);
+        market = findViewById(R.id.textView14);
+        volume = findViewById(R.id.textView15);
+        search = findViewById(R.id.mSearch);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search();
+            }
+        });
+
+        showCoin(coin);
+    }
+
+    private void showCoin(Coin coin) {
+        name.setText(coin.getName());
+        shortName.setText(coin.getSymbol());
+        value.setText(String.valueOf(coin.getValue()));
+        change1h.setText(String.valueOf(coin.getChange1h()));
+        change24h.setText(String.valueOf(coin.getChange24h()));
+        change7d.setText(String.valueOf(coin.getChange7d()));
+        market.setText(String.valueOf(coin.getMarketcap()));
+        volume.setText(String.valueOf(coin.getVolume()));
+    }
+
+    private void search() {
+        String url = "https://www.google.com/";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,49 +1,45 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+
     public static final String EXTRA_MESSAGE = "au.edu.unsw.infs3634.beers.MESSAGE";
-    private Button mButton;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate Called:");
         setContentView(R.layout.activity_main);
 
-        mButton = findViewById(R.id.clickMe);
-        mButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                launchDetailActivity("Hello from DetailActivity ***");
-            }
-        });
-
-
+        mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        CoinAdapter.RecyclerViewClickListener listener = new
+                CoinAdapter.RecyclerViewClickListener() {
+                    @Override
+                    public void onClick(View view, int position) {
+                        launchDetailActivity(position);
+                    }
+                };
+        mAdapter = new CoinAdapter(Coin.getCoins(), listener);
+        mRecyclerView.setAdapter(mAdapter);
     }
-    private void launchDetailActivity(String message){
-        Intent intent = new Intent( this, ActivityNEW.class);
+
+    private void launchDetailActivity(int message){
+        Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart Called");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume Called");
-    }
 }
